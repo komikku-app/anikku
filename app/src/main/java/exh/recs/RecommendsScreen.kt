84de07b0042cbe8234213df0d9f0bdcbc9b11538
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-01-21 20:29:32 +0100

    feat: unify recommendation screens and add more sources (jobobby04/TachiyomiSY#1376)
    * feat(recommendations): add mangaupdates.com support
    
    * feat(recommendations): display all tracker recommendation sources
    
    * refactor(recommendations): apply spotless
    
    * refactor: split RecommendationPagingSources
    
    * feat(recommendations): unify MangaDex & community recommendations
    
    * refactor: remove old screen
    
    * fix: update comment
    
    * style: fix formatting
    
    * refactor: move onClick handlers
    
    * fix: handle external recommendation links correctly
    
    * fix: apply spotless
    
    * feat: add comick recommendation source
    
    * fix: mark recs from comick as not initialized to force fetching missing metadata
    
    * Update app/src/main/java/exh/recs/BrowseRecommendsScreen.kt
    
    ---------
    
    Co-authored-by: jobobby04 <jobobby04@users.noreply.github.com>
    (cherry picked from commit d80ad3f145868f9c79bc6d94c08ed847c650e6d6)

--- app/src/main/java/exh/recs/RecommendsScreen.kt
+++ app/src/main/java/exh/recs/RecommendsScreen.kt
@@ -1,23 +1,16 @@
 package exh.recs
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import eu.kanade.core.util.ifSourcesLoaded
-import eu.kanade.presentation.browse.BrowseSourceContent
-import eu.kanade.presentation.browse.components.BrowseSourceSimpleToolbar
-import eu.kanade.presentation.components.BulkSelectionToolbar
 import eu.kanade.presentation.util.Screen
 import eu.kanade.tachiyomi.ui.browse.AddDuplicateMangaDialog
 import eu.kanade.tachiyomi.ui.browse.AllowDuplicateDialog
@@ -26,10 +19,11 @@ import eu.kanade.tachiyomi.ui.browse.ChangeMangaCategoryDialog
 import eu.kanade.tachiyomi.ui.browse.ChangeMangasCategoryDialog
 import eu.kanade.tachiyomi.ui.browse.RemoveMangaDialog
 import eu.kanade.tachiyomi.ui.browse.source.SourcesScreen
-import mihon.presentation.core.util.collectAsLazyPagingItems
+import eu.kanade.tachiyomi.ui.manga.MangaScreen
+import eu.kanade.tachiyomi.ui.webview.WebViewActivity
+import exh.recs.components.RecommendsScreen
 import tachiyomi.core.common.util.lang.launchIO
 import tachiyomi.domain.manga.model.Manga
-import tachiyomi.presentation.core.components.material.Scaffold
 import tachiyomi.presentation.core.screens.LoadingScreen
 
 class RecommendsScreen(val mangaId: Long, val sourceId: Long) : Screen() {
@@ -41,8 +35,13 @@ class RecommendsScreen(val mangaId: Long, val sourceId: Long) : Screen() {
             return
         }
 
-        val screenModel = rememberScreenModel { RecommendsScreenModel(mangaId, sourceId) }
+        val context = LocalContext.current
         val navigator = LocalNavigator.currentOrThrow
+
+        val screenModel = rememberScreenModel {
+            RecommendsScreenModel(mangaId = mangaId, sourceId = sourceId)
+        }
+        val state by screenModel.state.collectAsState()
 
         // KMK -->
         val scope = rememberCoroutineScope()
@@ -56,92 +55,68 @@ class RecommendsScreen(val mangaId: Long, val sourceId: Long) : Screen() {
         }
         // KMK <--
 
-        val onMangaClick: (Manga) -> Unit = { manga ->
-            openSmartSearch(navigator, manga.ogTitle)
-        }
-
-        val snackbarHostState = remember { SnackbarHostState() }
-
-        // KMK -->
-        val mangaList = screenModel.mangaPagerFlowFlow.collectAsLazyPagingItems()
-        // KMK <--
-        Scaffold(
-            topBar = { scrollBehavior ->
-                // KMK -->
-                if (bulkFavoriteState.selectionMode) {
-                    BulkSelectionToolbar(
-                        selectedCount = bulkFavoriteState.selection.size,
-                        isRunning = bulkFavoriteState.isRunning,
-                        onClickClearSelection = bulkFavoriteScreenModel::toggleSelectionMode,
-                        onChangeCategoryClick = bulkFavoriteScreenModel::addFavorite,
-                        onSelectAll = {
-                            mangaList.itemSnapshotList.items
-                                .map { it.value }
-                                .forEach { manga ->
-                                    bulkFavoriteScreenModel.select(manga)
-                                }
-                        },
-                        onReverseSelection = {
-                            bulkFavoriteScreenModel.reverseSelection(
-                                mangaList.itemSnapshotList.items
-                                    .map { it.value },
-                            )
-                        },
-                    )
-                } else {
-                    // KMK <--
-                    BrowseSourceSimpleToolbar(
-                        navigateUp = navigator::pop,
-                        title = screenModel.manga.title,
-                        displayMode = screenModel.displayMode,
-                        onDisplayModeChange = { screenModel.displayMode = it },
-                        scrollBehavior = scrollBehavior,
-                        // KMK -->
-                        toggleSelectionMode = bulkFavoriteScreenModel::toggleSelectionMode,
-                        isRunning = bulkFavoriteState.isRunning,
-                        // KMK <--
-                    )
-                }
-            },
-            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
-        ) { paddingValues ->
-            BrowseSourceContent(
-                source = screenModel.source,
-                mangaList = mangaList,
-                columns = screenModel.getColumnsPreference(LocalConfiguration.current.orientation),
-                displayMode = screenModel.displayMode,
-                snackbarHostState = snackbarHostState,
-                contentPadding = paddingValues,
-                onWebViewClick = null,
-                onHelpClick = null,
-                onLocalSourceHelpClick = null,
-                onMangaClick = {
-                    // KMK -->
-                    scope.launchIO {
-                        val manga = screenModel.networkToLocalManga.getLocal(it)
-                        if (bulkFavoriteState.selectionMode) {
-                            bulkFavoriteScreenModel.toggleSelection(manga)
-                        } else {
-                            // KMK <--
-                            onMangaClick(manga)
-                        }
-                    }
+        val onClickItem = { manga: Manga ->
+            navigator.push(
+                when (manga.source) {
+                    -1L -> SourcesScreen(SourcesScreen.SmartSearchConfig(manga.ogTitle))
+                    else -> MangaScreen(manga.id, true)
                 },
-                onMangaLongClick = {
-                    // KMK -->
-                    scope.launchIO {
-                        val manga = screenModel.networkToLocalManga.getLocal(it)
-                        if (!bulkFavoriteState.selectionMode) {
-                            bulkFavoriteScreenModel.addRemoveManga(manga, haptic)
-                        }
-                    }
-                    // KMK <--
-                },
-                // KMK -->
-                selection = bulkFavoriteState.selection,
-                // KMK <--
             )
         }
+
+        val onLongClickItem = { manga: Manga ->
+            when (manga.source) {
+                -1L -> WebViewActivity.newIntent(context, manga.url, title = manga.title).let(context::startActivity)
+                else -> onClickItem(manga)
+            }
+        }
+
+        RecommendsScreen(
+            manga = screenModel.manga,
+            state = state,
+            navigateUp = navigator::pop,
+            getManga = @Composable { manga: Manga -> screenModel.getManga(manga) },
+            onClickSource = { pagingSource ->
+                // Pass class name of paging source as screens need to be serializable
+                navigator.push(
+                    BrowseRecommendsScreen(
+                        mangaId,
+                        sourceId,
+                        pagingSource::class.qualifiedName!!,
+                        pagingSource.associatedSourceId == null,
+                    ),
+                )
+            },
+            onClickItem = {
+                // KMK -->
+                scope.launchIO {
+                    val manga = screenModel.networkToLocalManga.getLocal(it)
+                    if (bulkFavoriteState.selectionMode) {
+                        bulkFavoriteScreenModel.toggleSelection(manga)
+                    } else {
+                        // KMK <--
+                        onClickItem(manga)
+                    }
+                }
+            },
+            onLongClickItem = {
+                // KMK -->
+                scope.launchIO {
+                    val manga = screenModel.networkToLocalManga.getLocal(it)
+                    if (!bulkFavoriteState.selectionMode) {
+                        bulkFavoriteScreenModel.addRemoveManga(manga, haptic)
+                    } else {
+                        // KMK <--
+                        onLongClickItem(manga)
+                    }
+                }
+            },
+            // KMK -->
+            // bulkFavoriteScreenModel = bulkFavoriteScreenModel,
+            // displayMode = screenModel.displayMode,
+            // onDisplayModeChange = { screenModel.displayMode = it },
+            // KMK <--
+        )
 
         // KMK -->
         when (bulkFavoriteState.dialog) {
@@ -158,13 +133,5 @@ class RecommendsScreen(val mangaId: Long, val sourceId: Long) : Screen() {
             else -> {}
         }
         // KMK <--
-    }
-
-    /**
-     * Called when click on an recommending entry to search sources for it.
-     */
-    private fun openSmartSearch(navigator: Navigator, title: String) {
-        val smartSearchConfig = SourcesScreen.SmartSearchConfig(title)
-        navigator.push(SourcesScreen(smartSearchConfig))
     }
 }
