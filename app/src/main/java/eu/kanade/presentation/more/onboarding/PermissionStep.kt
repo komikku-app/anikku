065a4b095c53ab7262dedf1c0972ca11734140bf
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2025-02-26 17:30:02 +1300

    Stop showing divider when analytics is not included (mihonapp/mihon#1778)
    (cherry picked from commit 8b28a9bceeb43330d9b42cf2b9a98a072cf9aa19)

--- app/src/main/java/eu/kanade/presentation/more/onboarding/PermissionStep.kt
+++ app/src/main/java/eu/kanade/presentation/more/onboarding/PermissionStep.kt
@@ -179,12 +179,12 @@ internal class PermissionStep : OnboardingStep {
             }
             // KMK <--
 
+            if (!analyticsIncluded) return@Column
+
             HorizontalDivider(
                 modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
                 color = MaterialTheme.colorScheme.onPrimaryContainer,
             )
-
-            if (!analyticsIncluded) return@Column
 
             val crashlyticsPref = privacyPreferences.crashlytics()
             val crashlytics by crashlyticsPref.collectAsState()
