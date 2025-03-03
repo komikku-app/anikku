6be071ff9f9c76a71e463c6f29d14478a64ce16b
Cuong-Tran <cuongtran.tm@gmail.com>
2025-03-03 12:21:43 +0700

    Merge query for UpdateView & LibraryView (komikku-app/komikku#741)
    Remove the inline-code and move it to database
    
    * merge queries for Updates view
    
    * merge queries for Library view
    
    * add comment
    
    * fix spotless
    
    * also migrate database
    
    (cherry picked from commit d4c4686ad19aeec302af926d2d78cdefba769361)

--- source-api/src/commonMain/kotlin/exh/source/SourceIds.kt
+++ source-api/src/commonMain/kotlin/exh/source/SourceIds.kt
@@ -3,4 +3,5 @@ package exh.source
 // Lewd source IDs
 const val LEWD_SOURCE_SERIES = 6900L
 
+// If MERGED_SOURCE_ID is to update, remember to update updatesView.sq and libraryView.sq too
 const val MERGED_SOURCE_ID = LEWD_SOURCE_SERIES + 69
