1c8e22b01d5528893c477e92cb36d0aecadd2d60
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-03-19 05:16:03 +0600

    For release builds use last commit time as build time (mihonapp/mihon#1873)
    (cherry picked from commit dae7d179662ff6d6654e7c10e57f1aeeaf579de8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
             isShrinkResources = Config.enableCodeShrink
 
             proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+
+            buildConfigField("String", "BUILD_TIME", "\"${getBuildTime(useLastCommitTime = true)}\"")
         }
 
         val commonMatchingFallbacks = listOf(release.name)
