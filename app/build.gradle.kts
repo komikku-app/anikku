5babbc9931ae3ba7ace8b882f00ea4bfa0e3e858
Secozzi <49240133+Secozzi@users.noreply.github.com>
2025-04-01 08:52:15 +0200

    Add markdown support for manga descriptions (mihonapp/mihon#1948)
    (cherry picked from commit 4e68339783b47b0780e1b9aee643404339d35ed1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -302,7 +302,6 @@ dependencies {
         exclude(group = "androidx.viewpager", module = "viewpager")
     }
     implementation(libs.insetter)
-    implementation(libs.bundles.richtext)
     implementation(libs.richeditor.compose)
     implementation(libs.aboutLibraries.compose)
     implementation(libs.bundles.voyager)
@@ -311,6 +310,7 @@ dependencies {
     implementation(libs.compose.webview)
     implementation(libs.compose.grid)
     implementation(libs.reorderable)
+    implementation(libs.bundles.markdown)
 
     // KMK -->
     implementation(libs.palette.ktx)
