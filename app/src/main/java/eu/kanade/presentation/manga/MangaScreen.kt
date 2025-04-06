0fdfe3508978881c22d2df741d741fa98573459d
Cuong-Tran <cuongtran.tm@gmail.com>
2025-04-06 17:48:50 +0700

    fix(ui): NaN when dragging `Start/Resume` reading button in MangaScreen
    (cherry picked from commit ea418b9517a05b0dcc68bfbbba4315ff1067c832)

--- app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
@@ -548,7 +548,10 @@ private fun MangaScreenSmallImpl(
                             },
                         ) { change, dragAmount ->
                             change.consume()
-                            offsetX += dragAmount
+                            val newOffsetX = offsetX + dragAmount
+                            if (!newOffsetX.isNaN()) {
+                                offsetX = newOffsetX
+                            }
                         }
                     },
                 // KMK <--
@@ -1008,7 +1011,10 @@ private fun MangaScreenLargeImpl(
                             },
                         ) { change, dragAmount ->
                             change.consume()
-                            offsetX += dragAmount
+                            val newOffsetX = offsetX + dragAmount
+                            if (!newOffsetX.isNaN()) {
+                                offsetX = newOffsetX
+                            }
                         }
                     },
                 // KMK <--
