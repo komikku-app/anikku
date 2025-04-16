Ramsesdb/gexu-legacy-fork

Make more sliders discrete and ensure they don't look out of place (mihonapp/mihon#1840)
Also cleanup the underlying code

(cherry picked from commit 4f06c1cc09d15245b26b8a862738cb6a859fedcc)

app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
@@ -317,9 +317,9 @@ private fun ColumnScope.DisplayPage(
 
         val columns by columnPreference.collectAsState()
         SliderItem(
-            label = stringResource(MR.strings.pref_library_columns),
-            max = 10,
             value = columns,
+            valueRange = 0..10,
+            label = stringResource(MR.strings.pref_library_columns),
             valueText = if (columns > 0) {
                 columns.toString()
             } else {

komikku-app/anikku

app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
@@ -319,9 +319,9 @@ private fun ColumnScope.DisplayPage(
     val columns by columnPreference.collectAsState()
     if (displayMode == LibraryDisplayMode.List) {
         SliderItem(
-            label = stringResource(AMR.strings.pref_library_items_per_screen),
-            max = 10,
             value = columns,
+            valueRange = 0..10,
+            label = stringResource(AMR.strings.pref_library_items_per_screen),
             valueText = if (columns > 0) {
                 columns.toString()
             } else {
@@ -332,9 +332,9 @@ private fun ColumnScope.DisplayPage(
         )
     } else {
         SliderItem(
-            label = stringResource(AMR.strings.pref_library_items_per_row),
-            max = 10,
             value = columns,
+            valueRange = 0..10,
+            label = stringResource(AMR.strings.pref_library_items_per_row),
             valueText = if (columns > 0) {
                 columns.toString()
             } else {