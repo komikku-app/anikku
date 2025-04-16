Ramsesdb/gexu

Remove extensions repo

domain/src/main/java/mihon/domain/extensionrepo/interactor/CreateExtensionRepo.kt
@@ -71,11 +71,10 @@ class CreateExtensionRepo(
     }
 
     companion object {
-        const val OFFICIAL_REPO_WEBSITE = "https://komikku-app.github.io"
-        const val OFFICIAL_REPO_BASE_URL = "https://raw.githubusercontent.com/komikku-app/extensions/repo"
+        const val REPO_HELP = "https://komikku-app.github.io/docs/guides/getting-started#adding-sources"
 
         // cuong-tran's key
-        const val OFFICIAL_REPO_SIGNATURE = "cbec121aa82ebb02aaa73806992e0368a97d47b5451ed6524816d03084c45905"
-        const val KEIYOUSHI_REPO_SIGNATURE = "9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2"
+        const val KOMIKKU_SIGNATURE = "cbec121aa82ebb02aaa73806992e0368a97d47b5451ed6524816d03084c45905"
+        const val KEIYOUSHI_SIGNATURE = "9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2"
     }
 }

komikku-app/anikku

(cherry picked from commit 9d2a030bf588937b8f2a056d05998b4ceff5a587)

domain/src/main/java/mihon/domain/extensionrepo/interactor/CreateExtensionRepo.kt
@@ -71,11 +71,10 @@ class CreateExtensionRepo(
     }
 
     companion object {
-        const val OFFICIAL_REPO_WEBSITE = "https://anikku-app.github.io"
-        const val OFFICIAL_REPO_BASE_URL = "https://raw.githubusercontent.com/zosetsu-repo/ani-repo/repo"
+        const val REPO_HELP = "https://anikku-app.github.io/docs/guides/getting-started#adding-sources"
 
         // cuong-tran's key
-        const val OFFICIAL_REPO_SIGNATURE = "cbec121aa82ebb02aaa73806992e0368a97d47b5451ed6524816d03084c45905"
-        const val KEIYOUSHI_REPO_SIGNATURE = "9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2"
+        const val KOMIKKU_SIGNATURE = "cbec121aa82ebb02aaa73806992e0368a97d47b5451ed6524816d03084c45905"
+        const val KEIYOUSHI_SIGNATURE = "9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2"
     }
 }