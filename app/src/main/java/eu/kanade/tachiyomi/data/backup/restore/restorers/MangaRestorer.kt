komikku-app/komikku

Fix (MangaRestorer): avoid exception caused by duplicate entries
Duplicate chapters or duplicate merge-reference which would leads to the whole manga failed to restore/add to library

app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -393,7 +393,10 @@ class MangaRestorer(
 
     private suspend fun restoreHistory(manga: Manga, backupHistory: List<BackupHistory>) {
         val toUpdate = backupHistory.mapNotNull { history ->
-            val dbHistory = handler.awaitOneOrNull { historyQueries.getHistoryByChapterUrl(manga.id, history.url) }
+            // KMK -->
+            val dbHistory = handler.awaitList { historyQueries.getHistoryByChapterUrl(manga.id, history.url) }
+                .firstOrNull()
+            // KMK <--
             val item = history.getHistoryImpl()
 
             if (dbHistory == null) {
@@ -514,13 +517,18 @@ class MangaRestorer(
                 }
             ) {
                 // Let the db assign the id
-                val mergedManga = handler.awaitOneOrNull {
+                // KMK -->
+                val mergedManga = handler.awaitList {
+                    // KMK <--
                     mangasQueries.getMangaByUrlAndSource(
                         backupMergedMangaReference.mangaUrl,
                         backupMergedMangaReference.mangaSourceId,
                         MangaMapper::mapManga,
                     )
-                } ?: return@forEach
+                    // KMK -->
+                }.firstOrNull()
+                    // KMK <--
+                    ?: return@forEach
                 backupMergedMangaReference.getMergedMangaReference().run {
                     handler.await {
                         mergedQueries.insert(

komikku-app/anikku

(cherry picked from commit caa0d9b92e52af8c13a99ca1fdbc050520432ad8)

app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -392,7 +392,10 @@ class MangaRestorer(
 
     private suspend fun restoreHistory(manga: Manga, backupHistory: List<BackupHistory>) {
         val toUpdate = backupHistory.mapNotNull { history ->
-            val dbHistory = handler.awaitOneOrNull { historyQueries.getHistoryByEpisodeUrl(manga.id, history.url) }
+            // KMK -->
+            val dbHistory = handler.awaitList { historyQueries.getHistoryByEpisodeUrl(manga.id, history.url) }
+                .firstOrNull()
+            // KMK <--
             val item = history.getHistoryImpl()
 
             if (dbHistory == null) {
@@ -513,13 +516,18 @@ class MangaRestorer(
                 }
             ) {
                 // Let the db assign the id
-                val mergedManga = handler.awaitOneOrNull {
+                // KMK -->
+                val mergedManga = handler.awaitList {
+                    // KMK <--
                     animesQueries.getAnimeByUrlAndSource(
                         backupMergedMangaReference.mangaUrl,
                         backupMergedMangaReference.mangaSourceId,
                         MangaMapper::mapManga,
                     )
-                } ?: return@forEach
+                    // KMK -->
+                }.firstOrNull()
+                    // KMK <--
+                    ?: return@forEach
                 backupMergedMangaReference.getMergedMangaReference().run {
                     handler.await {
                         mergedQueries.insert(