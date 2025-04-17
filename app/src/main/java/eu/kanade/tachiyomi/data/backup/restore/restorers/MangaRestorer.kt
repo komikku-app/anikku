komikku-app/komikku

chore (MangaRestorer): use getChapterByUrlAndMangaId directly instead of getChapterByUrl then find

app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -397,8 +397,10 @@ class MangaRestorer(
             val item = history.getHistoryImpl()
 
             if (dbHistory == null) {
-                val chapter = handler.awaitList { chaptersQueries.getChapterByUrl(history.url) }
-                    .find { it.manga_id == manga.id }
+                // KMK -->
+                val chapter = handler.awaitList { chaptersQueries.getChapterByUrlAndMangaId(history.url, manga.id) }
+                    .firstOrNull()
+                // KMK <--
                 return@mapNotNull if (chapter == null) {
                     // Chapter doesn't exist; skip
                     null

komikku-app/anikku

(cherry picked from commit 58dc3b3e79968027c02dff111e2b17310cef40b1)

app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -396,8 +396,10 @@ class MangaRestorer(
             val item = history.getHistoryImpl()
 
             if (dbHistory == null) {
-                val chapter = handler.awaitList { episodesQueries.getEpisodeByUrl(history.url) }
-                    .find { it.anime_id == manga.id }
+                // KMK -->
+                val chapter = handler.awaitList { episodesQueries.getEpisodeByUrlAndAnimeId(history.url, manga.id) }
+                    .firstOrNull()
+                // KMK <--
                 return@mapNotNull if (chapter == null) {
                     // Chapter doesn't exist; skip
                     null