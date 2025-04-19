komikku-app/komikku

Add back option to hide unread chapter badge in library (mihonapp/mihon#1871)
(cherry picked from commit ac432e2e941f4689caad246bab6aa7d303c83bfa)

app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
@@ -336,6 +336,10 @@ private fun ColumnScope.DisplayPage(
         pref = screenModel.libraryPreferences.downloadBadge(),
     )
     CheckboxItem(
+        label = stringResource(MR.strings.action_display_unread_badge),
+        pref = screenModel.libraryPreferences.unreadBadge(),
+    )
+    CheckboxItem(
         label = stringResource(MR.strings.action_display_local_badge),
         pref = screenModel.libraryPreferences.localBadge(),
     )

app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
@@ -506,6 +506,7 @@ class LibraryScreenModel(
     private fun getLibraryItemPreferencesFlow(): Flow<ItemPreferences> {
         return combine(
             libraryPreferences.downloadBadge().changes(),
+            libraryPreferences.unreadBadge().changes(),
             libraryPreferences.localBadge().changes(),
             libraryPreferences.languageBadge().changes(),
             libraryPreferences.autoUpdateMangaRestrictions().changes(),
@@ -527,22 +528,23 @@ class LibraryScreenModel(
         ) {
             ItemPreferences(
                 downloadBadge = it[0] as Boolean,
-                localBadge = it[1] as Boolean,
-                languageBadge = it[2] as Boolean,
-                skipOutsideReleasePeriod = LibraryPreferences.MANGA_OUTSIDE_RELEASE_PERIOD in (it[3] as Set<*>),
-                globalFilterDownloaded = it[4] as Boolean,
-                filterDownloaded = it[5] as TriState,
-                filterUnread = it[6] as TriState,
-                filterStarted = it[7] as TriState,
-                filterBookmarked = it[8] as TriState,
-                filterCompleted = it[9] as TriState,
-                filterIntervalCustom = it[10] as TriState,
+                unreadBadge = it[1] as Boolean,
+                localBadge = it[2] as Boolean,
+                languageBadge = it[3] as Boolean,
+                skipOutsideReleasePeriod = LibraryPreferences.MANGA_OUTSIDE_RELEASE_PERIOD in (it[4] as Set<*>),
+                globalFilterDownloaded = it[5] as Boolean,
+                filterDownloaded = it[6] as TriState,
+                filterUnread = it[7] as TriState,
+                filterStarted = it[8] as TriState,
+                filterBookmarked = it[9] as TriState,
+                filterCompleted = it[10] as TriState,
+                filterIntervalCustom = it[11] as TriState,
                 // SY -->
-                filterLewd = it[11] as TriState,
+                filterLewd = it[12] as TriState,
                 // SY <--
                 // KMK -->
-                sourceBadge = it[12] as Boolean,
-                useLangIcon = it[13] as Boolean,
+                sourceBadge = it[13] as Boolean,
+                useLangIcon = it[14] as Boolean,
                 // KMK <--
             )
         }
@@ -578,7 +580,7 @@ class LibraryScreenModel(
                         } else {
                             0
                         },
-                        unreadCount = libraryManga.unreadCount,
+                        unreadCount = if (prefs.unreadBadge) libraryManga.unreadCount else 0,
                         isLocal = if (prefs.localBadge) libraryManga.manga.isLocal() else false,
                         sourceLanguage = if (prefs.languageBadge) {
                             source.lang
@@ -1414,6 +1416,7 @@ class LibraryScreenModel(
     @Immutable
     private data class ItemPreferences(
         val downloadBadge: Boolean,
+        val unreadBadge: Boolean,
         val localBadge: Boolean,
         val languageBadge: Boolean,
         // KMK -->

komikku-app/anikku

app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
@@ -351,6 +351,10 @@ private fun ColumnScope.DisplayPage(
         pref = screenModel.libraryPreferences.downloadBadge(),
     )
     CheckboxItem(
+        label = stringResource(AMR.strings.action_display_unread_badge),
+        pref = screenModel.libraryPreferences.unreadBadge(),
+    )
+    CheckboxItem(
         label = stringResource(MR.strings.action_display_local_badge),
         pref = screenModel.libraryPreferences.localBadge(),
     )

app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
@@ -490,6 +490,7 @@ class LibraryScreenModel(
     private fun getLibraryItemPreferencesFlow(): Flow<ItemPreferences> {
         return combine(
             libraryPreferences.downloadBadge().changes(),
+            libraryPreferences.unreadBadge().changes(),
             libraryPreferences.localBadge().changes(),
             libraryPreferences.languageBadge().changes(),
             libraryPreferences.autoUpdateMangaRestrictions().changes(),
@@ -514,25 +515,26 @@ class LibraryScreenModel(
         ) {
             ItemPreferences(
                 downloadBadge = it[0] as Boolean,
-                localBadge = it[1] as Boolean,
-                languageBadge = it[2] as Boolean,
-                skipOutsideReleasePeriod = LibraryPreferences.ANIME_OUTSIDE_RELEASE_PERIOD in (it[3] as Set<*>),
-                globalFilterDownloaded = it[4] as Boolean,
-                filterDownloaded = it[5] as TriState,
-                filterUnread = it[6] as TriState,
-                filterStarted = it[7] as TriState,
-                filterBookmarked = it[8] as TriState,
+                unreadBadge = it[1] as Boolean,
+                localBadge = it[2] as Boolean,
+                languageBadge = it[3] as Boolean,
+                skipOutsideReleasePeriod = LibraryPreferences.ANIME_OUTSIDE_RELEASE_PERIOD in (it[4] as Set<*>),
+                globalFilterDownloaded = it[5] as Boolean,
+                filterDownloaded = it[6] as TriState,
+                filterUnread = it[7] as TriState,
+                filterStarted = it[8] as TriState,
+                filterBookmarked = it[9] as TriState,
                 // AM (FILLERMARK) -->
-                filterFillermarked = it[9] as TriState,
+                filterFillermarked = it[10] as TriState,
                 // <-- AM (FILLERMARK)
-                filterCompleted = it[10] as TriState,
-                filterIntervalCustom = it[11] as TriState,
+                filterCompleted = it[11] as TriState,
+                filterIntervalCustom = it[12] as TriState,
                 // SY -->
-                filterLewd = it[12] as TriState,
+                filterLewd = it[13] as TriState,
                 // SY <--
                 // KMK -->
-                sourceBadge = it[13] as Boolean,
-                useLangIcon = it[14] as Boolean,
+                sourceBadge = it[14] as Boolean,
+                useLangIcon = it[15] as Boolean,
                 // KMK <--
             )
         }
@@ -568,7 +570,7 @@ class LibraryScreenModel(
                         } else {
                             0
                         },
-                        unreadCount = libraryManga.unreadCount,
+                        unreadCount = if (prefs.unreadBadge) libraryManga.unreadCount else 0,
                         isLocal = if (prefs.localBadge) libraryManga.manga.isLocal() else false,
                         sourceLanguage = if (prefs.languageBadge) {
                             source.lang
@@ -1319,6 +1321,7 @@ class LibraryScreenModel(
     @Immutable
     private data class ItemPreferences(
         val downloadBadge: Boolean,
+        val unreadBadge: Boolean,
         val localBadge: Boolean,
         val languageBadge: Boolean,
         // KMK -->