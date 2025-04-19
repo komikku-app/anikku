Ramsesdb/gexu-legacy-fork

Change backup proto & improve code of backup/restore category related preferences (mihonapp/mihon#1726)
Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>
(cherry picked from commit e1724d1aa0e3340e1404cfd80bd264831d86a879)

domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
@@ -26,38 +26,29 @@ class DownloadPreferences(
 
     fun removeBookmarkedChapters() = preferenceStore.getBoolean("pref_remove_bookmarked", false)
 
-    fun removeExcludeCategories() = preferenceStore.getStringSet(
-        // KMK -->
-        REMOVE_EXCLUDE_CATEGORIES_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun removeExcludeCategories() = preferenceStore.getStringSet(REMOVE_EXCLUDE_CATEGORIES_PREF_KEY, emptySet())
 
     fun downloadNewChapters() = preferenceStore.getBoolean("download_new", false)
 
-    fun downloadNewChapterCategories() = preferenceStore.getStringSet(
-        // KMK -->
-        DOWNLOAD_NEW_CATEGORIES_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun downloadNewChapterCategories() = preferenceStore.getStringSet(DOWNLOAD_NEW_CATEGORIES_PREF_KEY, emptySet())
 
-    fun downloadNewChapterCategoriesExclude() = preferenceStore.getStringSet(
-        // KMK -->
-        DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun downloadNewChapterCategoriesExclude() =
+        preferenceStore.getStringSet(DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY, emptySet())
 
     fun downloadNewUnreadChaptersOnly() = preferenceStore.getBoolean("download_new_unread_chapters_only", false)
 
     // KMK -->
     fun downloadCacheRenewInterval() = preferenceStore.getInt("download_cache_renew_interval", 1)
+    // KMK <--
 
     companion object {
-        const val REMOVE_EXCLUDE_CATEGORIES_PREF_KEY = "remove_exclude_categories"
-        const val DOWNLOAD_NEW_CATEGORIES_PREF_KEY = "download_new_categories"
-        const val DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY = "download_new_categories_exclude"
+        private const val REMOVE_EXCLUDE_CATEGORIES_PREF_KEY = "remove_exclude_categories"
+        private const val DOWNLOAD_NEW_CATEGORIES_PREF_KEY = "download_new_categories"
+        private const val DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY = "download_new_categories_exclude"
+        val categoryPreferenceKeys = setOf(
+            REMOVE_EXCLUDE_CATEGORIES_PREF_KEY,
+            DOWNLOAD_NEW_CATEGORIES_PREF_KEY,
+            DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY,
+        )
     }
-    // KMK <--
 }

domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
@@ -144,12 +144,7 @@ class LibraryPreferences(
 
     // region Category
 
-    fun defaultCategory() = preferenceStore.getInt(
-        // KMK -->
-        DEFAULT_CATEGORY_PREF_KEY,
-        // KMK <--
-        -1,
-    )
+    fun defaultCategory() = preferenceStore.getInt(DEFAULT_CATEGORY_PREF_KEY, -1)
 
     fun lastUsedCategory() = preferenceStore.getInt(Preference.appStateKey("last_used_category"), 0)
 
@@ -163,19 +158,9 @@ class LibraryPreferences(
     fun showHiddenCategories() = preferenceStore.getBoolean("hide_hidden_categories", false)
     // KMK <--
 
-    fun updateCategories() = preferenceStore.getStringSet(
-        // KMK -->
-        LIBRARY_UPDATE_CATEGORIES_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun updateCategories() = preferenceStore.getStringSet(LIBRARY_UPDATE_CATEGORIES_PREF_KEY, emptySet())
 
-    fun updateCategoriesExclude() = preferenceStore.getStringSet(
-        // KMK -->
-        LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun updateCategoriesExclude() = preferenceStore.getStringSet(LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY, emptySet())
 
     // endregion
 
@@ -268,10 +253,13 @@ class LibraryPreferences(
         const val MANGA_NON_READ = "manga_started"
         const val MANGA_OUTSIDE_RELEASE_PERIOD = "manga_outside_release_period"
 
-        // KMK -->
         const val DEFAULT_CATEGORY_PREF_KEY = "default_category"
-        const val LIBRARY_UPDATE_CATEGORIES_PREF_KEY = "library_update_categories"
-        const val LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY = "library_update_categories_exclude"
-        // KMK <--
+        private const val LIBRARY_UPDATE_CATEGORIES_PREF_KEY = "library_update_categories"
+        private const val LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY = "library_update_categories_exclude"
+        val categoryPreferenceKeys = setOf(
+            DEFAULT_CATEGORY_PREF_KEY,
+            LIBRARY_UPDATE_CATEGORIES_PREF_KEY,
+            LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY,
+        )
     }
 }

komikku-app/anikku

domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
@@ -33,38 +33,29 @@ class DownloadPreferences(
     fun notDownloadFillermarkedItems() = preferenceStore.getBoolean("pref_no_download_fillermarked", false)
     // <-- AM (FILLERMARK)
 
-    fun removeExcludeCategories() = preferenceStore.getStringSet(
-        // KMK -->
-        REMOVE_EXCLUDE_CATEGORIES_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun removeExcludeCategories() = preferenceStore.getStringSet(REMOVE_EXCLUDE_CATEGORIES_PREF_KEY, emptySet())
 
     fun downloadNewChapters() = preferenceStore.getBoolean("download_new_episode", false)
 
-    fun downloadNewChapterCategories() = preferenceStore.getStringSet(
-        // KMK -->
-        DOWNLOAD_NEW_CATEGORIES_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun downloadNewChapterCategories() = preferenceStore.getStringSet(DOWNLOAD_NEW_CATEGORIES_PREF_KEY, emptySet())
 
-    fun downloadNewChapterCategoriesExclude() = preferenceStore.getStringSet(
-        // KMK -->
-        DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun downloadNewChapterCategoriesExclude() =
+        preferenceStore.getStringSet(DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY, emptySet())
 
     fun downloadNewUnreadChaptersOnly() = preferenceStore.getBoolean("download_new_unread_episodes_only", false)
 
     // KMK -->
     fun downloadCacheRenewInterval() = preferenceStore.getInt("download_cache_renew_interval", 1)
+    // KMK <--
 
     companion object {
-        const val REMOVE_EXCLUDE_CATEGORIES_PREF_KEY = "remove_exclude_anime_categories"
-        const val DOWNLOAD_NEW_CATEGORIES_PREF_KEY = "download_new_anime_categories"
-        const val DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY = "download_new_anime_categories_exclude"
+        private const val REMOVE_EXCLUDE_CATEGORIES_PREF_KEY = "remove_exclude_anime_categories"
+        private const val DOWNLOAD_NEW_CATEGORIES_PREF_KEY = "download_new_anime_categories"
+        private const val DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY = "download_new_anime_categories_exclude"
+        val categoryPreferenceKeys = setOf(
+            REMOVE_EXCLUDE_CATEGORIES_PREF_KEY,
+            DOWNLOAD_NEW_CATEGORIES_PREF_KEY,
+            DOWNLOAD_NEW_CATEGORIES_EXCLUDE_PREF_KEY,
+        )
     }
-    // KMK <--
 }

domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
@@ -153,12 +153,7 @@ class LibraryPreferences(
 
     // region Category
 
-    fun defaultCategory() = preferenceStore.getInt(
-        // KMK -->
-        DEFAULT_CATEGORY_PREF_KEY,
-        // KMK <--
-        -1,
-    )
+    fun defaultCategory() = preferenceStore.getInt(DEFAULT_CATEGORY_PREF_KEY, -1)
 
     fun lastUsedCategory() = preferenceStore.getInt(Preference.appStateKey("last_used_anime_category"), 0)
 
@@ -172,19 +167,9 @@ class LibraryPreferences(
     fun showHiddenCategories() = preferenceStore.getBoolean("show_hidden_categories", false)
     // KMK <--
 
-    fun updateCategories() = preferenceStore.getStringSet(
-        // KMK -->
-        LIBRARY_UPDATE_CATEGORIES_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun updateCategories() = preferenceStore.getStringSet(LIBRARY_UPDATE_CATEGORIES_PREF_KEY, emptySet())
 
-    fun updateCategoriesExclude() = preferenceStore.getStringSet(
-        // KMK -->
-        LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY,
-        // KMK <--
-        emptySet(),
-    )
+    fun updateCategoriesExclude() = preferenceStore.getStringSet(LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY, emptySet())
 
     // Mixture Item
 
@@ -282,10 +267,13 @@ class LibraryPreferences(
         const val ANIME_NON_SEEN = "anime_started"
         const val ANIME_OUTSIDE_RELEASE_PERIOD = "anime_outside_release_period"
 
-        // KMK -->
         const val DEFAULT_CATEGORY_PREF_KEY = "default_anime_category"
-        const val LIBRARY_UPDATE_CATEGORIES_PREF_KEY = "animelib_update_categories"
-        const val LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY = "animelib_update_categories_exclude"
-        // KMK <--
+        private const val LIBRARY_UPDATE_CATEGORIES_PREF_KEY = "animelib_update_categories"
+        private const val LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY = "animelib_update_categories_exclude"
+        val categoryPreferenceKeys = setOf(
+            DEFAULT_CATEGORY_PREF_KEY,
+            LIBRARY_UPDATE_CATEGORIES_PREF_KEY,
+            LIBRARY_UPDATE_CATEGORIES_EXCLUDE_PREF_KEY,
+        )
     }
 }