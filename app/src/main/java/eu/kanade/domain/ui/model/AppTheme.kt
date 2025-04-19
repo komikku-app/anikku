aniyomiorg/aniyomi

Implement i18nTail for improved string handling (#190)
* Implement i18nTail for improved string handling

* formatt

* Translations update from Crowdin

app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
@@ -2,11 +2,12 @@ package eu.kanade.domain.ui.model
 
 import dev.icerock.moko.resources.StringResource
 import tachiyomi.i18n.MR
+import tachiyomi.i18n.tail.TLMR
 
 enum class AppTheme(val titleRes: StringResource?) {
     DEFAULT(MR.strings.label_default),
     MONET(MR.strings.theme_monet),
-    CUSTOM(MR.strings.theme_custom),
+    CUSTOM(TLMR.strings.theme_custom),
     CLOUDFLARE(MR.strings.theme_cloudflare),
     COTTONCANDY(MR.strings.theme_cottoncandy),
     DOOM(MR.strings.theme_doom),

app/src/main/java/eu/kanade/presentation/more/settings/screen/data/SyncTriggerOptionsScreen.kt
@@ -15,6 +15,7 @@ import eu.kanade.tachiyomi.data.sync.models.SyncTriggerOptions
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.update
 import tachiyomi.i18n.MR
+import tachiyomi.i18n.tail.TLMR
 import tachiyomi.presentation.core.components.LabeledCheckbox
 import tachiyomi.presentation.core.components.LazyColumnWithAction
 import tachiyomi.presentation.core.components.SectionCard
@@ -34,7 +35,7 @@ class SyncTriggerOptionsScreen : Screen() {
         Scaffold(
             topBar = {
                 AppBar(
-                    title = stringResource(MR.strings.pref_sync_options),
+                    title = stringResource(TLMR.strings.pref_sync_options),
                     navigateUp = navigator::pop,
                     scrollBehavior = it,
                 )
@@ -49,7 +50,7 @@ class SyncTriggerOptionsScreen : Screen() {
                 },
             ) {
                 item {
-                    SectionCard(MR.strings.label_triggers) {
+                    SectionCard(TLMR.strings.label_triggers) {
                         Options(SyncTriggerOptions.mainOptions, state, model)
                     }
                 }

app/src/main/java/eu/kanade/tachiyomi/ui/setting/track/GoogleDriveLoginActivity.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.lifecycleScope
 import eu.kanade.tachiyomi.data.sync.service.GoogleDriveService
 import tachiyomi.core.common.i18n.stringResource
 import tachiyomi.core.common.util.lang.launchIO
-import tachiyomi.i18n.MR
+import tachiyomi.i18n.tail.TLMR
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
@@ -23,7 +23,7 @@ class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
                     onSuccess = {
                         Toast.makeText(
                             this@GoogleDriveLoginActivity,
-                            stringResource(MR.strings.google_drive_login_success),
+                            stringResource(TLMR.strings.google_drive_login_success),
                             Toast.LENGTH_LONG,
                         ).show()
 
@@ -32,7 +32,7 @@ class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
                     onFailure = { error ->
                         Toast.makeText(
                             this@GoogleDriveLoginActivity,
-                            stringResource(MR.strings.google_drive_login_failed, error),
+                            stringResource(TLMR.strings.google_drive_login_failed, error),
                             Toast.LENGTH_LONG,
                         ).show()
                         returnToSettings()
@@ -42,7 +42,7 @@ class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
         } else if (error != null) {
             Toast.makeText(
                 this@GoogleDriveLoginActivity,
-                stringResource(MR.strings.google_drive_login_failed, error),
+                stringResource(TLMR.strings.google_drive_login_failed, error),
                 Toast.LENGTH_LONG,
             ).show()
 

komikku-app/anikku

(cherry picked from commit 986869ff6858d8468b43d76d4e4fb6323332cb0c)

app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
@@ -2,11 +2,12 @@ package eu.kanade.domain.ui.model
 
 import dev.icerock.moko.resources.StringResource
 import tachiyomi.i18n.MR
+import tachiyomi.i18n.kmk.KMR
 
 enum class AppTheme(val titleRes: StringResource?) {
     DEFAULT(MR.strings.label_default),
     MONET(MR.strings.theme_monet),
-    CUSTOM(MR.strings.theme_custom),
+    CUSTOM(KMR.strings.theme_custom),
     CLOUDFLARE(MR.strings.theme_cloudflare),
     COTTONCANDY(MR.strings.theme_cottoncandy),
     DOOM(MR.strings.theme_doom),

app/src/main/java/eu/kanade/presentation/more/settings/screen/data/SyncTriggerOptionsScreen.kt
@@ -15,6 +15,7 @@ import eu.kanade.tachiyomi.data.sync.models.SyncTriggerOptions
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.update
 import tachiyomi.i18n.MR
+import tachiyomi.i18n.sy.SYMR
 import tachiyomi.presentation.core.components.LabeledCheckbox
 import tachiyomi.presentation.core.components.LazyColumnWithAction
 import tachiyomi.presentation.core.components.SectionCard
@@ -34,7 +35,7 @@ class SyncTriggerOptionsScreen : Screen() {
         Scaffold(
             topBar = {
                 AppBar(
-                    title = stringResource(MR.strings.pref_sync_options),
+                    title = stringResource(SYMR.strings.pref_sync_options),
                     navigateUp = navigator::pop,
                     scrollBehavior = it,
                 )
@@ -49,7 +50,7 @@ class SyncTriggerOptionsScreen : Screen() {
                 },
             ) {
                 item {
-                    SectionCard(MR.strings.label_triggers) {
+                    SectionCard(SYMR.strings.label_triggers) {
                         Options(SyncTriggerOptions.mainOptions, state, model)
                     }
                 }

app/src/main/java/eu/kanade/tachiyomi/ui/setting/track/GoogleDriveLoginActivity.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.lifecycleScope
 import eu.kanade.tachiyomi.data.sync.service.GoogleDriveService
 import tachiyomi.core.common.i18n.stringResource
 import tachiyomi.core.common.util.lang.launchIO
-import tachiyomi.i18n.MR
+import tachiyomi.i18n.sy.SYMR
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
@@ -23,7 +23,7 @@ class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
                     onSuccess = {
                         Toast.makeText(
                             this@GoogleDriveLoginActivity,
-                            stringResource(MR.strings.google_drive_login_success),
+                            stringResource(SYMR.strings.google_drive_login_success),
                             Toast.LENGTH_LONG,
                         ).show()
 
@@ -32,7 +32,7 @@ class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
                     onFailure = { error ->
                         Toast.makeText(
                             this@GoogleDriveLoginActivity,
-                            stringResource(MR.strings.google_drive_login_failed, error),
+                            stringResource(SYMR.strings.google_drive_login_failed, error),
                             Toast.LENGTH_LONG,
                         ).show()
                         returnToSettings()
@@ -42,7 +42,7 @@ class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
         } else if (error != null) {
             Toast.makeText(
                 this@GoogleDriveLoginActivity,
-                stringResource(MR.strings.google_drive_login_failed, error),
+                stringResource(SYMR.strings.google_drive_login_failed, error),
                 Toast.LENGTH_LONG,
             ).show()
 