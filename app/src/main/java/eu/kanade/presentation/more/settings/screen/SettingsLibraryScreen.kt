komikku-app/komikku

Add option to mark new duplicate read chapters as read (mihonapp/mihon#1785)
(cherry picked from commit cd0481592c09dc9cfb331805e90e6e5c3752a08c)

app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsLibraryScreen.kt
@@ -231,6 +231,10 @@ object SettingsLibraryScreen : SearchableSettings {
                     preference = libraryPreferences.newShowUpdatesCount(),
                     title = stringResource(MR.strings.pref_library_update_show_tab_badge),
                 ),
+                Preference.PreferenceItem.SwitchPreference(
+                    preference = libraryPreferences.markDuplicateChapterRead(),
+                    title = stringResource(MR.strings.pref_mark_duplicate_chapter_read),
+                ),
                 // KMK -->
                 Preference.PreferenceItem.SwitchPreference(
                     preference = libraryPreferences.showUpdatingProgressBanner(),

domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
@@ -152,6 +152,8 @@ class LibraryPreferences(
 
     fun categorizedDisplaySettings() = preferenceStore.getBoolean("categorized_display", false)
 
+    fun markDuplicateChapterRead() = preferenceStore.getBoolean("mark_duplicate_chapter_read", false)
+
     // KMK -->
     fun showHiddenCategories() = preferenceStore.getBoolean("hide_hidden_categories", false)
     // KMK <--

komikku-app/anikku

app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsLibraryScreen.kt
@@ -231,6 +231,10 @@ object SettingsLibraryScreen : SearchableSettings {
                     preference = libraryPreferences.newShowUpdatesCount(),
                     title = stringResource(AMR.strings.pref_library_update_show_tab_badge_episode),
                 ),
+                Preference.PreferenceItem.SwitchPreference(
+                    preference = libraryPreferences.markDuplicateEpisodeSeen(),
+                    title = stringResource(AMR.strings.pref_mark_duplicate_episode_seen),
+                ),
                 // KMK -->
                 Preference.PreferenceItem.SwitchPreference(
                     preference = libraryPreferences.showUpdatingProgressBanner(),

domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
@@ -119,8 +119,6 @@ class LibraryPreferences(
         "pref_filter_library_lewd_v2",
         TriState.DISABLED,
     )
-
-    fun libraryReadDuplicateChapters() = preferenceStore.getBoolean("pref_library_mark_duplicate_chapters", false)
     // SY <--
 
     fun filterTracking(id: Int) = preferenceStore.getEnum(
@@ -162,6 +160,8 @@ class LibraryPreferences(
     fun categoryNumberOfItems() = preferenceStore.getBoolean("display_number_of_items", false)
 
     fun categorizedDisplaySettings() = preferenceStore.getBoolean("categorized_display", false)
+
+    fun markDuplicateEpisodeSeen() = preferenceStore.getBoolean("mark_duplicate_episode_seen", false)
 
     // KMK -->
     fun showHiddenCategories() = preferenceStore.getBoolean("show_hidden_categories", false)