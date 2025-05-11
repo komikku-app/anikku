komikku-app/komikku

Migrate to Bangumi's newer v0 API (mihonapp/mihon#1748)
This comes with many benefits:
- Starting dates are now available and shown to users
- Lays groundwork to add private tracking for Bangumi, e.g. in mihonapp/mihon#1736
- Mihon makes approximately 2-4 times fewer calls to Bangumi's API
- Simplified interceptor for the access token addition
  - v0 does not allow access tokens in the query string
- There is actively maintained documentation for it

Also shrunk the DTOs for Bangumi by removing attributes we have no
use for either now or in the foreseeable future. Volume data remains
in case Mihon wants to ever support volumes. But attributes such as
user avatars, nicknames, data relating to Bangumi's tag & meta-tag
systems, etc. have been removed or just not added to the DTOs.

Co-authored-by: cuong-tran <cuongtran.tm@gmail.com>
(cherry picked from commit a96fbba3dc354e363b85923c52feceb88dc34447)

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/Bangumi.kt
@@ -10,7 +10,6 @@ import eu.kanade.tachiyomi.data.track.model.TrackMangaMetadata
 import eu.kanade.tachiyomi.data.track.model.TrackSearch
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import tachiyomi.i18n.MR
 import uy.kohesive.injekt.injectLazy
@@ -49,26 +48,23 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     }
 
     override suspend fun bind(track: Track, hasReadChapters: Boolean): Track {
-        val statusTrack = api.statusLibManga(track)
-        val remoteTrack = api.findLibManga(track)
-        return if (remoteTrack != null && statusTrack != null) {
-            track.copyPersonalFrom(remoteTrack)
-            track.library_id = remoteTrack.library_id
-
+        val statusTrack = api.statusLibManga(track, getUsername())
+        return if (statusTrack != null) {
+            track.copyPersonalFrom(statusTrack)
+            track.library_id = statusTrack.library_id
+            track.score = statusTrack.score
+            track.last_chapter_read = statusTrack.last_chapter_read
+            track.total_chapters = statusTrack.total_chapters
             if (track.status != COMPLETED) {
                 track.status = if (hasReadChapters) READING else statusTrack.status
             }
 
-            track.score = statusTrack.score
-            track.last_chapter_read = statusTrack.last_chapter_read
-            track.total_chapters = remoteTrack.total_chapters
-            refresh(track)
+            track
         } else {
             // Set default fields if it's not found in the list
             track.status = if (hasReadChapters) READING else PLAN_TO_READ
             track.score = 0.0
             add(track)
-            update(track)
         }
     }
 
@@ -81,11 +77,8 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     }
 
     override suspend fun refresh(track: Track): Track {
-        val remoteStatusTrack = api.statusLibManga(track) ?: throw Exception("Could not find manga")
+        val remoteStatusTrack = api.statusLibManga(track, getUsername()) ?: throw Exception("Could not find manga")
         track.copyPersonalFrom(remoteStatusTrack)
-        api.findLibManga(track)?.let { remoteTrack ->
-            track.total_chapters = remoteTrack.total_chapters
-        }
         return track
     }
 
@@ -117,9 +110,13 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     suspend fun login(code: String) {
         try {
             val oauth = api.accessToken(code)
+            // Users can set a 'username' (not nickname) once which effectively
+            // replaces the string field ID in certain queries.
+            // If no username is set, the API returns the user ID as a strings
+            val username = api.getUsername()
             interceptor.newAuth(oauth)
-            saveCredentials(oauth.userId.toString(), oauth.accessToken)
-        } catch (e: Throwable) {
+            saveCredentials(username, oauth.accessToken)
+        } catch (_: Throwable) {
             logout()
         }
     }
@@ -131,7 +128,7 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     fun restoreToken(): BGMOAuth? {
         return try {
             json.decodeFromString<BGMOAuth>(trackPreferences.trackToken(this).get())
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             null
         }
     }
@@ -143,11 +140,11 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     }
 
     companion object {
-        const val READING = 3L
+        const val PLAN_TO_READ = 1L
         const val COMPLETED = 2L
+        const val READING = 3L
         const val ON_HOLD = 4L
         const val DROPPED = 5L
-        const val PLAN_TO_READ = 1L
 
         private val SCORE_LIST = IntRange(0, 10)
             .map(Int::toString)

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiUtils.kt
@@ -3,10 +3,10 @@ package eu.kanade.tachiyomi.data.track.bangumi
 import eu.kanade.tachiyomi.data.database.models.Track
 
 fun Track.toApiStatus() = when (status) {
-    Bangumi.READING -> "do"
-    Bangumi.COMPLETED -> "collect"
-    Bangumi.ON_HOLD -> "on_hold"
-    Bangumi.DROPPED -> "dropped"
-    Bangumi.PLAN_TO_READ -> "wish"
-    else -> "do"
+    Bangumi.PLAN_TO_READ -> 1
+    Bangumi.COMPLETED -> 2
+    Bangumi.READING -> 3
+    Bangumi.ON_HOLD -> 4
+    Bangumi.DROPPED -> 5
+    else -> 3
 }

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/dto/BGMCollectionResponse.kt
@@ -1,28 +1,34 @@
 package eu.kanade.tachiyomi.data.track.bangumi.dto
 
+import eu.kanade.tachiyomi.data.track.bangumi.Bangumi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+// Incomplete DTO with only our needed attributes
 data class BGMCollectionResponse(
-    val code: Int?,
-    val `private`: Int? = 0,
-    val comment: String? = "",
+    val rate: Int?,
+    val type: Int?,
     @SerialName("ep_status")
     val epStatus: Int? = 0,
-    @SerialName("lasttouch")
-    val lastTouch: Int? = 0,
-    val rating: Double? = 0.0,
-    val status: Status? = Status(),
-    val tag: List<String?>? = emptyList(),
-    val user: User? = User(),
     @SerialName("vol_status")
     val volStatus: Int? = 0,
-)
+    val private: Boolean = false,
+    val subject: BGMSlimSubject? = null,
+) {
+    fun getStatus(): Long = when (type) {
+        1 -> Bangumi.PLAN_TO_READ
+        2 -> Bangumi.COMPLETED
+        3 -> Bangumi.READING
+        4 -> Bangumi.ON_HOLD
+        5 -> Bangumi.DROPPED
+        else -> throw NotImplementedError("Unknown status: $type")
+    }
+}
 
 @Serializable
-data class Status(
-    val id: Long? = 0,
-    val name: String? = "",
-    val type: String? = "",
+// Incomplete DTO with only our needed attributes
+data class BGMSlimSubject(
+    val volumes: Int?,
+    val eps: Int?,
 )

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/dto/BGMSearch.kt
@@ -6,45 +6,53 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BGMSearchResult(
-    val list: List<BGMSearchItem>?,
-    val code: Int?,
+    val total: Int,
+    val limit: Int,
+    val offset: Int,
+    val data: List<BGMSubject> = emptyList(),
 )
 
 @Serializable
-data class BGMSearchItem(
+// Incomplete DTO with only our needed attributes
+data class BGMSubject(
     val id: Long,
     @SerialName("name_cn")
     val nameCn: String,
     val name: String,
-    val type: Int,
     val summary: String?,
-    val images: BGMSearchItemCovers?,
-    @SerialName("eps_count")
-    val epsCount: Long?,
-    val rating: BGMSearchItemRating?,
-    val url: String,
+    val date: String?, // YYYY-MM-DD
+    val images: BGMSubjectImages?,
+    val volumes: Long = 0,
+    val eps: Long = 0,
+    val rating: BGMSubjectRating?,
+    // SY -->
+    val infobox: List<Infobox>,
+    // SY <--
 ) {
     fun toTrackSearch(trackId: Long): TrackSearch = TrackSearch.create(trackId).apply {
-        remote_id = this@BGMSearchItem.id
+        remote_id = this@BGMSubject.id
         title = nameCn.ifBlank { name }
         cover_url = images?.common.orEmpty()
         summary = if (nameCn.isNotBlank()) {
-            "作品原名：$name" + this@BGMSearchItem.summary?.let { "\n$it" }.orEmpty()
+            "作品原名：$name" + this@BGMSubject.summary?.let { "\n${it.trim()}" }.orEmpty()
         } else {
-            this@BGMSearchItem.summary.orEmpty()
+            this@BGMSubject.summary?.trim().orEmpty()
         }
         score = rating?.score ?: -1.0
-        tracking_url = url
-        total_chapters = epsCount ?: 0
+        tracking_url = "https://bangumi.tv/subject/${this@BGMSubject.id}"
+        total_chapters = eps
+        start_date = date ?: ""
     }
 }
 
 @Serializable
-data class BGMSearchItemCovers(
+// Incomplete DTO with only our needed attributes
+data class BGMSubjectImages(
     val common: String?,
 )
 
 @Serializable
-data class BGMSearchItemRating(
+// Incomplete DTO with only our needed attributes
+data class BGMSubjectRating(
     val score: Double?,
 )

komikku-app/anikku

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/Bangumi.kt
@@ -49,27 +49,23 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     }
 
     override suspend fun bind(track: Track, hasReadChapters: Boolean): Track {
-        val statusTrack = api.statusLibAnime(track)
-        val remoteTrack = api.findLibAnime(track)
+        val statusTrack = api.statusLibAnime(track, getUsername())
         return if (statusTrack != null) {
-            track.copyPersonalFrom(remoteTrack)
-            track.library_id = remoteTrack.library_id
-
+            track.copyPersonalFrom(statusTrack)
+            track.library_id = statusTrack.library_id
+            track.score = statusTrack.score
+            track.last_episode_seen = statusTrack.last_episode_seen
+            track.total_episodes = statusTrack.total_episodes
             if (track.status != COMPLETED) {
                 track.status = if (hasReadChapters) WATCHING else statusTrack.status
             }
 
-            // track.status = statusTrack.status
-            track.score = statusTrack.score
-            track.last_episode_seen = statusTrack.last_episode_seen
-            track.total_episodes = remoteTrack.total_episodes
-            refresh(track)
+            track
         } else {
             // Set default fields if it's not found in the list
             track.status = if (hasReadChapters) WATCHING else PLAN_TO_WATCH
             track.score = 0.0
             add(track)
-            update(track)
         }
     }
 
@@ -82,11 +78,8 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     }
 
     override suspend fun refresh(track: Track): Track {
-        val remoteStatusTrack = api.statusLibAnime(track) ?: throw Exception("Could not find anime")
+        val remoteStatusTrack = api.statusLibAnime(track, getUsername()) ?: throw Exception("Could not find manga")
         track.copyPersonalFrom(remoteStatusTrack)
-        api.findLibAnime(track).let { remoteTrack ->
-            track.total_episodes = remoteTrack.total_episodes
-        }
         return track
     }
 
@@ -118,9 +111,13 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     suspend fun login(code: String) {
         try {
             val oauth = api.accessToken(code)
+            // Users can set a 'username' (not nickname) once which effectively
+            // replaces the stringified ID in certain queries.
+            // If no username is set, the API returns the user ID as a strings
+            val username = api.getUsername()
             interceptor.newAuth(oauth)
-            saveCredentials(oauth.userId.toString(), oauth.accessToken)
-        } catch (e: Throwable) {
+            saveCredentials(username, oauth.accessToken)
+        } catch (_: Throwable) {
             logout()
         }
     }
@@ -132,7 +129,7 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     fun restoreToken(): BGMOAuth? {
         return try {
             json.decodeFromString<BGMOAuth>(trackPreferences.trackToken(this).get())
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             null
         }
     }
@@ -144,11 +141,11 @@ class Bangumi(id: Long) : BaseTracker(id, "Bangumi") {
     }
 
     companion object {
-        const val WATCHING = 3L
+        const val PLAN_TO_WATCH = 1L
         const val COMPLETED = 2L
+        const val WATCHING = 3L
         const val ON_HOLD = 4L
         const val DROPPED = 5L
-        const val PLAN_TO_WATCH = 1L
 
         private val SCORE_LIST = IntRange(0, 10)
             .map(Int::toString)

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiUtils.kt
@@ -3,10 +3,10 @@ package eu.kanade.tachiyomi.data.track.bangumi
 import eu.kanade.tachiyomi.data.database.models.Track
 
 fun Track.toApiStatus() = when (status) {
-    Bangumi.WATCHING -> "do"
-    Bangumi.COMPLETED -> "collect"
-    Bangumi.ON_HOLD -> "on_hold"
-    Bangumi.DROPPED -> "dropped"
-    Bangumi.PLAN_TO_WATCH -> "wish"
-    else -> "do"
+    Bangumi.PLAN_TO_WATCH -> 1
+    Bangumi.COMPLETED -> 2
+    Bangumi.WATCHING -> 3
+    Bangumi.ON_HOLD -> 4
+    Bangumi.DROPPED -> 5
+    else -> 3
 }

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/dto/BGMCollectionResponse.kt
@@ -1,28 +1,34 @@
 package eu.kanade.tachiyomi.data.track.bangumi.dto
 
+import eu.kanade.tachiyomi.data.track.bangumi.Bangumi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+// Incomplete DTO with only our needed attributes
 data class BGMCollectionResponse(
-    val code: Int?,
-    val `private`: Int? = 0,
-    val comment: String? = "",
+    val rate: Int?,
+    val type: Int?,
     @SerialName("ep_status")
     val epStatus: Int? = 0,
-    @SerialName("lasttouch")
-    val lastTouch: Int? = 0,
-    val rating: Double? = 0.0,
-    val status: Status? = Status(),
-    val tag: List<String?>? = emptyList(),
-    val user: User? = User(),
     @SerialName("vol_status")
     val volStatus: Int? = 0,
-)
+    val private: Boolean = false,
+    val subject: BGMSlimSubject? = null,
+) {
+    fun getStatus(): Long = when (type) {
+        1 -> Bangumi.PLAN_TO_WATCH
+        2 -> Bangumi.COMPLETED
+        3 -> Bangumi.WATCHING
+        4 -> Bangumi.ON_HOLD
+        5 -> Bangumi.DROPPED
+        else -> throw NotImplementedError("Unknown status: $type")
+    }
+}
 
 @Serializable
-data class Status(
-    val id: Long? = 0,
-    val name: String? = "",
-    val type: String? = "",
+// Incomplete DTO with only our needed attributes
+data class BGMSlimSubject(
+    val volumes: Int?,
+    val eps: Int?,
 )

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/dto/BGMSearch.kt
@@ -6,41 +6,55 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BGMSearchResult(
-    val list: List<BGMSearchItem>?,
-    val code: Int?,
+    val total: Int,
+    val limit: Int,
+    val offset: Int,
+    val data: List<BGMSubject> = emptyList(),
 )
 
 @Serializable
-data class BGMSearchItem(
+// Incomplete DTO with only our needed attributes
+data class BGMSubject(
     val id: Long,
     @SerialName("name_cn")
     val nameCn: String,
     val name: String,
-    val type: Int,
     val summary: String?,
-    val images: BGMSearchItemCovers?,
-    @SerialName("eps_count")
-    val epsCount: Long?,
-    val rating: BGMSearchItemRating?,
-    val url: String,
+    val date: String?, // YYYY-MM-DD
+    val images: BGMSubjectImages?,
+    val volumes: Long = 0,
+    val eps: Long = 0,
+    val rating: BGMSubjectRating?,
+    // KMK -->
+    val tags: List<BGMTag>,
+    // KMK <---
+    // SY -->
+    val infobox: List<Infobox>,
+    // SY <--
 ) {
     fun toTrackSearch(trackId: Long): TrackSearch = TrackSearch.create(trackId).apply {
-        remote_id = this@BGMSearchItem.id
+        remote_id = this@BGMSubject.id
         title = nameCn.ifBlank { name }
         cover_url = images?.common.orEmpty()
         summary = if (nameCn.isNotBlank()) {
-            "作品原名：$name" + this@BGMSearchItem.summary?.let { "\n$it" }.orEmpty()
+            "作品原名：$name" + this@BGMSubject.summary?.let { "\n${it.trim()}" }.orEmpty()
         } else {
-            this@BGMSearchItem.summary.orEmpty()
+            this@BGMSubject.summary?.trim().orEmpty()
         }
         score = rating?.score ?: -1.0
-        tracking_url = url
-        total_episodes = epsCount ?: 0
+        tracking_url = "https://bangumi.tv/subject/${this@BGMSubject.id}"
+        total_episodes = eps
+        start_date = date ?: ""
     }
 }
 
 @Serializable
-data class BGMSearchItemCovers(
+data class BGMTag(
+    val name: String? = null,
+)
+
+@Serializable
+data class BGMSubjectImages(
     val large: String?,
     val medium: String?,
     val common: String?,
@@ -49,6 +63,7 @@ data class BGMSearchItemCovers(
 )
 
 @Serializable
-data class BGMSearchItemRating(
+// Incomplete DTO with only our needed attributes
+data class BGMSubjectRating(
     val score: Double?,
 )