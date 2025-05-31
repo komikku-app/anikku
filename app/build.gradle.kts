702375be05f5e6352fc487341ee2072ee92f6a6e
Cuong-Tran <cuongtran.tm@gmail.com>
2025-05-31 09:24:47 +0700

    Bump release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "app.anikku"
 
         versionCode = 3
-        versionName = "0.1.2"
+        versionName = "0.1.3"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
