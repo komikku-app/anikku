komikku-app/komikku

Don't hardcode app name in `strings.xml` (mihonapp/mihon#2394)
(cherry picked from commit 3d3c36078a3b8cb741adffd46931aeab9cb38578)

app/src/main/java/eu/kanade/tachiyomi/data/download/Downloader.kt
@@ -311,7 +311,10 @@ class Downloader(
                     maxDownloadsFromSource > CHAPTERS_PER_SOURCE_QUEUE_WARNING_THRESHOLD
                 ) {
                     notifier.onWarning(
-                        context.stringResource(MR.strings.download_queue_size_warning),
+                        context.stringResource(
+                            MR.strings.download_queue_size_warning,
+                            context.stringResource(MR.strings.app_name),
+                        ),
                         WARNING_NOTIF_TIMEOUT_MS,
                         NotificationHandler.openUrl(context, LibraryUpdateNotifier.HELP_WARNING_URL),
                     )

komikku-app/anikku

(cherry picked from commit 7d06e3b2110c86464328d8ac48cd42751332bd21)

app/src/main/java/eu/kanade/tachiyomi/data/download/Downloader.kt
@@ -318,7 +318,10 @@ class Downloader(
                     maxDownloadsFromSource > EPISODES_PER_SOURCE_QUEUE_WARNING_THRESHOLD
                 ) {
                     notifier.onWarning(
-                        context.stringResource(AYMR.strings.download_queue_size_warning),
+                        context.stringResource(
+                            AYMR.strings.download_queue_size_warning,
+                            context.stringResource(MR.strings.app_name),
+                        ),
                         WARNING_NOTIF_TIMEOUT_MS,
                         NotificationHandler.openUrl(
                             context,