30d5e7af9cb88ae8975c6276e9c139268e1c6171
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-12-01 12:00:00 +0700

    feat(theme): Support Dynamic theme for Android 11 and below (komikku-app/komikku#1504)
    (cherry picked from commit 7380e77b5c85739f6eac74b3aaad960090892191)

--- app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
+++ app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
@@ -7,8 +7,6 @@ import eu.kanade.domain.ui.model.AppTheme
 import eu.kanade.domain.ui.model.StartScreen
 import eu.kanade.domain.ui.model.TabletUiMode
 import eu.kanade.domain.ui.model.ThemeMode
-import eu.kanade.tachiyomi.util.system.DeviceUtil
-import eu.kanade.tachiyomi.util.system.isDynamicColorAvailable
 import tachiyomi.core.common.preference.PreferenceStore
 import tachiyomi.core.common.preference.getEnum
 import java.time.format.DateTimeFormatter
@@ -23,11 +21,7 @@ class UiPreferences(
 
     fun appTheme() = preferenceStore.getEnum(
         "pref_app_theme",
-        if (DeviceUtil.isDynamicColorAvailable) {
-            AppTheme.MONET
-        } else {
-            AppTheme.DEFAULT
-        },
+        AppTheme.MONET,
     )
 
     fun themeDarkAmoled() = preferenceStore.getBoolean("pref_theme_dark_amoled_key", false)
