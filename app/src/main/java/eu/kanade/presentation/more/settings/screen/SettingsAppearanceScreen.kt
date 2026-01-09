e2a8feb6f0fc8ac374db56d4584c881c921d45bd
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-01-09 14:30:16 +0700

    Revert part of "Switch to MaterialExpressiveTheme" (komikku-app/komikku#1377)
    Co-authored-by: Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
    (cherry picked from commit 3e6afee13b9fd5716f7f2d547b3edcd3e17915db)
    
    This reverts part of commit 3ab83f1270075a871e68242d3d08876dfad619b1.
    
    (cherry picked from commit 9136c15b9e25dac15255fb7a361336daba0184fb)

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAppearanceScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAppearanceScreen.kt
@@ -116,14 +116,14 @@ object SettingsAppearanceScreen : SearchableSettings {
                     entries = PaletteStyle.entries
                         .associateWith {
                             when (it) {
-                                PaletteStyle.Expressive ->
-                                    stringResource(KMR.strings.pref_theme_cover_based_style_expressive)
                                 PaletteStyle.TonalSpot ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_tonalspot)
                                 PaletteStyle.Neutral ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_neutral)
                                 PaletteStyle.Vibrant ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_vibrant)
+                                PaletteStyle.Expressive ->
+                                    stringResource(KMR.strings.pref_theme_cover_based_style_expressive)
                                 PaletteStyle.Rainbow ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_rainbow)
                                 PaletteStyle.FruitSalad ->
@@ -179,14 +179,14 @@ object SettingsAppearanceScreen : SearchableSettings {
                     entries = PaletteStyle.entries
                         .associateWith {
                             when (it) {
-                                PaletteStyle.Expressive ->
-                                    stringResource(KMR.strings.pref_theme_cover_based_style_expressive)
                                 PaletteStyle.TonalSpot ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_tonalspot)
                                 PaletteStyle.Neutral ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_neutral)
                                 PaletteStyle.Vibrant ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_vibrant)
+                                PaletteStyle.Expressive ->
+                                    stringResource(KMR.strings.pref_theme_cover_based_style_expressive)
                                 PaletteStyle.Rainbow ->
                                     stringResource(KMR.strings.pref_theme_cover_based_style_rainbow)
                                 PaletteStyle.FruitSalad ->
