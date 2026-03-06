komikku-app/komikku

Add Filters to Updates screen (mihonapp/mihon#2851)
* Add Filters to Updates screen

Behaves basically like the filters in the library:

- Unread: Show/Don't show unread chapters
- Downloaded: Show/Don't show downloaded chapters
- Started: Show/Don't show chapters that have some progress but aren't
  fully Read
- Bookmarked: Show/Don't show chapters that have been bookmarked

Started behaves differently from its Library counterpart because the
actual manga data is not available at this point in time and I thought
calling getManga for each entry without caching would be a pretty bad
idea.

I have modelled this closely on the filter control flow in the
Library, but I'm sure this can be simplified/adjusted in some way.

* Move most filtering logic to SQL

Unread, Started, and Bookmarked filters are now part of the SQL query.

Download state cannot be filtered in the database so it remains in
Kotlin.

Because the Downloaded filter has to be run in Kotlin, the combine
flow uses the preferences flow twice, once to get the SQL query params
and once for the Kotlin filters (only Downloaded at this time).

* Add "Hide excluded scanlators" to update filters

Based on the work done in mihonapp/mihon#1623 but integrated with the other filters
in this PR. Added the user as a co-author for credit.

Co-authored-by: Dani <17619547+shabnix@users.noreply.github.com>

---------

Co-authored-by: Dani <17619547+shabnix@users.noreply.github.com>

---------

* Merge Panorama toggle into Filter diaglog
* Make Panorama toggle persistent and share it with History
* Implement toggleSwitch method for preference management to avoid create new function on every composition

Co-authored-by: Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>

(cherry picked from commit bbe9aa8561360f030072fbc49f79748e71c6535e)

app/src/main/java/eu/kanade/presentation/updates/UpdatesFilterDialog.kt
@@ -1,0 +1,139 @@
+package eu.kanade.presentation.updates
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import eu.kanade.presentation.components.TabbedDialog
+import eu.kanade.presentation.components.TabbedDialogPaddings
+import eu.kanade.tachiyomi.ui.updates.UpdatesSettingsScreenModel
+import kotlinx.collections.immutable.persistentListOf
+import tachiyomi.domain.updates.service.UpdatesPreferences
+import tachiyomi.i18n.MR
+import tachiyomi.i18n.kmk.KMR
+import tachiyomi.presentation.core.components.SettingsItemsPaddings
+import tachiyomi.presentation.core.components.TriStateItem
+import tachiyomi.presentation.core.components.material.padding
+import tachiyomi.presentation.core.i18n.stringResource
+import tachiyomi.presentation.core.util.collectAsState
+
+@Composable
+fun UpdatesFilterDialog(
+    onDismissRequest: () -> Unit,
+    screenModel: UpdatesSettingsScreenModel,
+) {
+    TabbedDialog(
+        onDismissRequest = onDismissRequest,
+        tabTitles = persistentListOf(
+            stringResource(MR.strings.action_filter),
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = TabbedDialogPaddings.Vertical)
+                .verticalScroll(rememberScrollState()),
+        ) {
+            FilterSheet(screenModel = screenModel)
+        }
+    }
+}
+
+@Composable
+private fun ColumnScope.FilterSheet(
+    screenModel: UpdatesSettingsScreenModel,
+) {
+    val filterDownloaded by screenModel.updatesPreferences.filterDownloaded().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.label_downloaded),
+        state = filterDownloaded,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterDownloaded) },
+    )
+
+    val filterUnread by screenModel.updatesPreferences.filterUnread().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.action_filter_unread),
+        state = filterUnread,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterUnread) },
+    )
+
+    val filterStarted by screenModel.updatesPreferences.filterStarted().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.label_started),
+        state = filterStarted,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterStarted) },
+    )
+
+    val filterBookmarked by screenModel.updatesPreferences.filterBookmarked().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.action_filter_bookmarked),
+        state = filterBookmarked,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterBookmarked) },
+    )
+
+    HorizontalDivider(modifier = Modifier.padding(MaterialTheme.padding.small))
+
+    val filterExcludedScanlators by screenModel.updatesPreferences.filterExcludedScanlators().collectAsState()
+
+    Row(
+        modifier = Modifier
+            // KMK -->
+            .clickable { screenModel.toggleSwitch(UpdatesPreferences::filterExcludedScanlators) }
+            // KMK <--
+            .fillMaxWidth()
+            .padding(horizontal = SettingsItemsPaddings.Horizontal),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(MR.strings.action_filter_excluded_scanlators),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Switch(
+            checked = filterExcludedScanlators,
+            // KMK -->
+            onCheckedChange = { screenModel.toggleSwitch(UpdatesPreferences::filterExcludedScanlators) },
+            // KMK <--
+        )
+    }
+
+    // KMK -->
+    HorizontalDivider(modifier = Modifier.padding(MaterialTheme.padding.small))
+
+    val panoramaCover by screenModel.updatesPreferences.usePanoramaCover().collectAsState()
+
+    Row(
+        modifier = Modifier
+            .clickable { screenModel.toggleSwitch(UpdatesPreferences::usePanoramaCover) }
+            .fillMaxWidth()
+            .padding(horizontal = SettingsItemsPaddings.Horizontal),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(KMR.strings.action_panorama_cover),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Switch(
+            checked = panoramaCover,
+            onCheckedChange = { screenModel.toggleSwitch(UpdatesPreferences::usePanoramaCover) },
+        )
+    }
+    // KMK <--
+}

komikku-app/anikku

(cherry picked from commit 3e7dc5c41a1368b5f4050eb2295db58208a376a8)

app/src/main/java/eu/kanade/presentation/updates/UpdatesFilterDialog.kt
@@ -1,0 +1,140 @@
+package eu.kanade.presentation.updates
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import eu.kanade.presentation.components.TabbedDialog
+import eu.kanade.presentation.components.TabbedDialogPaddings
+import eu.kanade.tachiyomi.ui.updates.UpdatesSettingsScreenModel
+import kotlinx.collections.immutable.persistentListOf
+import tachiyomi.domain.updates.service.UpdatesPreferences
+import tachiyomi.i18n.MR
+import tachiyomi.i18n.aniyomi.AYMR
+import tachiyomi.i18n.kmk.KMR
+import tachiyomi.presentation.core.components.SettingsItemsPaddings
+import tachiyomi.presentation.core.components.TriStateItem
+import tachiyomi.presentation.core.components.material.padding
+import tachiyomi.presentation.core.i18n.stringResource
+import tachiyomi.presentation.core.util.collectAsState
+
+@Composable
+fun UpdatesFilterDialog(
+    onDismissRequest: () -> Unit,
+    screenModel: UpdatesSettingsScreenModel,
+) {
+    TabbedDialog(
+        onDismissRequest = onDismissRequest,
+        tabTitles = persistentListOf(
+            stringResource(MR.strings.action_filter),
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(vertical = TabbedDialogPaddings.Vertical)
+                .verticalScroll(rememberScrollState()),
+        ) {
+            FilterSheet(screenModel = screenModel)
+        }
+    }
+}
+
+@Composable
+private fun ColumnScope.FilterSheet(
+    screenModel: UpdatesSettingsScreenModel,
+) {
+    val filterDownloaded by screenModel.updatesPreferences.filterDownloaded().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.label_downloaded),
+        state = filterDownloaded,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterDownloaded) },
+    )
+
+    val filterUnread by screenModel.updatesPreferences.filterUnread().collectAsState()
+    TriStateItem(
+        label = stringResource(AYMR.strings.action_filter_unseen),
+        state = filterUnread,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterUnread) },
+    )
+
+    val filterStarted by screenModel.updatesPreferences.filterStarted().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.label_started),
+        state = filterStarted,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterStarted) },
+    )
+
+    val filterBookmarked by screenModel.updatesPreferences.filterBookmarked().collectAsState()
+    TriStateItem(
+        label = stringResource(MR.strings.action_filter_bookmarked),
+        state = filterBookmarked,
+        onClick = { screenModel.toggleFilter(UpdatesPreferences::filterBookmarked) },
+    )
+
+    HorizontalDivider(modifier = Modifier.padding(MaterialTheme.padding.small))
+
+    val filterExcludedScanlators by screenModel.updatesPreferences.filterExcludedScanlators().collectAsState()
+
+    Row(
+        modifier = Modifier
+            // KMK -->
+            .clickable { screenModel.toggleSwitch(UpdatesPreferences::filterExcludedScanlators) }
+            // KMK <--
+            .fillMaxWidth()
+            .padding(horizontal = SettingsItemsPaddings.Horizontal),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(MR.strings.action_filter_excluded_scanlators),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Switch(
+            checked = filterExcludedScanlators,
+            // KMK -->
+            onCheckedChange = { screenModel.toggleSwitch(UpdatesPreferences::filterExcludedScanlators) },
+            // KMK <--
+        )
+    }
+
+    // KMK -->
+    HorizontalDivider(modifier = Modifier.padding(MaterialTheme.padding.small))
+
+    val panoramaCover by screenModel.updatesPreferences.usePanoramaCover().collectAsState()
+
+    Row(
+        modifier = Modifier
+            .clickable { screenModel.toggleSwitch(UpdatesPreferences::usePanoramaCover) }
+            .fillMaxWidth()
+            .padding(horizontal = SettingsItemsPaddings.Horizontal),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(KMR.strings.action_panorama_cover),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Switch(
+            checked = panoramaCover,
+            onCheckedChange = { screenModel.toggleSwitch(UpdatesPreferences::usePanoramaCover) },
+        )
+    }
+    // KMK <--
+}