4b61c1ef5b1d2c490aa339785f41bf43ba1503a7
Ernest Hysa <takis312@hotmail.com>
2026-07-27 20:23:09 +0100

    fix(macos): remove duplicate android/androidx stubs from keiyoushi-utils (already in macos/src)

--- macos/keiyoushi-utils/src/main/kotlin/androidx/preference/PreferenceGroup.kt
+++
@@ -1,7 +0,0 @@
-package androidx.preference
-
-import android.content.Context
-
-open class PreferenceGroup(context: Context) : Preference(context) {
-    fun addPreference(preference: Preference) {}
-}
--- macos/keiyoushi-utils/src/main/kotlin/androidx/preference/PreferenceScreen.kt
+++
@@ -1,5 +0,0 @@
-package androidx.preference
-
-import android.content.Context
-
-class PreferenceScreen(context: Context) : PreferenceGroup(context)
